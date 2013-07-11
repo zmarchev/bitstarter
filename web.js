787b5fee46c0b74e8dad126483fde9ece8fb1c3a
Zdravko Marchev <marchev@acetech.com>
2013-07-11 02:22:33 +0000

    Updated web.js with reading from index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(fs.readFileSync(./index.html));
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
