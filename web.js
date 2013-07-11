ed51dd4d7e02f570ae74b2acfe58aedd514b120f
Zdravko Marchev <marchev@acetech.com>
2013-07-11 02:51:20 +0000

    Updated web.js with reading from index.html - updated

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync(./index.html));
+var buffer = new Buffer(fs.readFileSync("./index.html"));
 
 
 app.get('/', function(request, response) {
